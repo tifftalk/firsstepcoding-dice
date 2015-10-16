cf75a2f6d30f3b9e0b2d0568747a95c44faf27e7
Andy Tiffany <andy.tiffany@twintechs.com>
2015-10-16 12:57:17 -0700

    shares lesson

--- Dice.rb
+++ Dice.rb
@@ -1,1 +1,33 @@
-puts "hello"
+class Dice
+    @color
+    @faceUp
+
+    def initialize( color, faceUp )
+        setFaceUp(faceUp)
+        setColor(color)
+        describe
+    end
+
+    def setFaceUp( x )
+        if x > 6 or x < 1
+            puts "YOU CANNOT DO THAT!"
+        else
+            @faceUp = x
+        end
+    end
+
+    def roll()
+        @faceUp = (rand * 6).ceil
+
+        describe
+    end
+
+    def setColor( x )
+        @color = x
+    end
+
+    def describe
+        puts "My face up is " + @faceUp.to_s
+        puts "My color is " + @color
+    end
+end
